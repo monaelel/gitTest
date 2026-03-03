c3d15204c3b219ef677bb18f7321d707fc2da36e
monaelel <monaelel@hotmail.com>
2026-03-03 16:34:10 -0500

    just a test

---
+++ app/src/main/java/com/example/gittest/ManuelActivity.java
@@ -0,0 +1,20 @@
+package com.example.gittest;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ManuelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_manuel);
+
+    }
+}
